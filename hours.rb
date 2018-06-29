10b0b6e3afaf45db3eb7be7563a85e726972bdbd
Marc <marc.maninang@gmail.com>
2018-06-28 22:34:53 -0700

    Hours uploaded

---
+++ hours.rb
@@ -0,0 +1,10 @@
+# hours in a year
+puts "Hours in a year = " + (24*365).to_s;
+
+# minutes in a decade
+
+# how many seconds old are you
+
+# how many chocolates do you hope to eat in your life
+
+# If I am 1298 million seconds old, how old am I?
